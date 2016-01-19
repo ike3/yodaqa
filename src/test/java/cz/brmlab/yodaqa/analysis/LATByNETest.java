c9f6a100ac638e31a0bda7d6eac3f89c0fa4d620
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2016-01-19 12:27:47 +0300

    [AnswerAnalysis] LATByNETest

---
+++ src/test/java/cz/brmlab/yodaqa/analysis/LATByNETest.java
@@ -0,0 +1,81 @@
+package cz.brmlab.yodaqa.analysis;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.*;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.component.JCasConsumer_ImplBase;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.*;
+
+import cz.brmlab.yodaqa.SimpleQuestion;
+import cz.brmlab.yodaqa.analysis.ansscore.AnswerFV;
+import cz.brmlab.yodaqa.analysis.answer.*;
+import cz.brmlab.yodaqa.flow.MultiCASPipeline;
+import cz.brmlab.yodaqa.model.CandidateAnswer.AnswerInfo;
+import cz.brmlab.yodaqa.model.TyCor.*;
+import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
+
+/*
+ */
+public class LATByNETest {
+    private static String EXPECTED_OUTPUT;
+    private static String INPUT;
+
+    public static class Tested extends SimpleQuestion {
+
+
+        public void initCas(JCas jcas) {
+            try {
+                super.initCas(jcas);
+
+                AnswerInfo ai = new AnswerInfo(jcas);
+                ai.setCanonText("правило правая нога ппн");
+                ai.addToIndexes(jcas);
+                AnswerFV fv = new AnswerFV(ai);
+                ai.setFeatures(fv.toFSArray(jcas));
+
+                NamedEntity ne = new NamedEntity(jcas);
+                ne.setValue(INPUT);
+                ne.addToIndexes(jcas);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static class TestConsumer extends JCasConsumer_ImplBase {
+        @Override
+        public void process(JCas jcas) throws AnalysisEngineProcessException {
+            try {
+                for (NELAT lat : JCasUtil.select(jcas, NELAT.class)) {
+                    if (EXPECTED_OUTPUT.equals(lat.getText()))
+                        return;
+                }
+                Assert.fail();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Test
+    public void run() throws Exception {
+        AggregateBuilder builder = new AggregateBuilder();
+        builder.add(createPrimitiveDescription(LATByNE.class));
+
+        CollectionReaderDescription reader = createReaderDescription(
+                Tested.class,
+                SimpleQuestion.PARAM_LANGUAGE, "en",
+                SimpleQuestion.PARAM_INPUT, "игнор");
+
+        INPUT = "Россия";
+        EXPECTED_OUTPUT = "Россия";
+        MultiCASPipeline.runPipeline(reader, builder.createAggregateDescription(), createEngineDescription(TestConsumer.class));
+    }
+
+}
