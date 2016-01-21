59c73cae6e814441fa4f4573df030e137e4083eb
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2016-01-21 15:45:30 +0300

    [QuestionAnalysis] playing with uima-ext components

--- src/test/java/cz/brmlab/yodaqa/TokenizerLabTest.java
+++ src/test/java/cz/brmlab/yodaqa/TokenizerLabTest.java
@@ -6,19 +6,23 @@ import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDe
 import java.util.*;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.analysis_engine.*;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
-import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.*;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ExternalResourceDescription;
 import org.junit.*;
 
 import cz.brmlab.yodaqa.flow.MultiCASPipeline;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
 import ru.kfu.cll.uima.tokenizer.fstype.SW;
+import ru.kfu.itis.issst.uima.morph.dictionary.MorphDictionaryAPIFactory;
+import ru.kfu.itis.issst.uima.morph.lemmatizer.Lemmatizer;
+import ru.kfu.itis.issst.uima.segmentation.SentenceSplitter;
 import ru.kfu.itis.issst.uima.tokenizer.*;
 
 public class TokenizerLabTest {
@@ -81,6 +85,34 @@ public class TokenizerLabTest {
 
         EXPECTED_CLASS = Token.class;
         EXPECTED_OUTPUT = "big,lands";
+        MultiCASPipeline.runPipeline(reader, builder.createAggregateDescription(), createEngineDescription(TestConsumer.class));
+    }
+
+    @Test
+    @Ignore
+    /*
+     * Для запуска нужно положить dict.opcorpora.ser в корень yodaqa
+     */
+    public void lemmatizer() throws Exception {
+        AggregateBuilder builder = new AggregateBuilder();
+        builder.add(createPrimitiveDescription(InitialTokenizer.class));
+        builder.add(createPrimitiveDescription(PostTokenizer.class));
+        builder.add(createPrimitiveDescription(SentenceSplitter.class));
+        // TODO: Нужен парсер, который бы аннотировал нам org.opencorpora.cas.Word
+        AnalysisEngineDescription desc = createPrimitiveDescription(Lemmatizer.class);
+        //MorphDictionary morphDict = MorphDictionaryAPIFactory.getMorphDictionaryAPI().getCachedInstance().getResource();
+        ExternalResourceDescription morphDictDesc = MorphDictionaryAPIFactory.getMorphDictionaryAPI().getResourceDescriptionForCachedInstance();
+        ExternalResourceFactory.bindExternalResource(desc, "morphDictionary", morphDictDesc);
+        builder.add(desc);
+
+        CollectionReaderDescription reader = createReaderDescription(
+                Tested.class,
+                SimpleQuestion.PARAM_LANGUAGE, "en",
+                SimpleQuestion.PARAM_INPUT, "большие земли начинаются с маленьких");
+
+        EXPECTED_CLASS = Token.class;
+        EXPECTED_OUTPUT = "";
+
         MultiCASPipeline.runPipeline(reader, builder.createAggregateDescription(), createEngineDescription(TestConsumer.class));
     }
 }
